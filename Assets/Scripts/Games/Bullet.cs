c8f4765e03d4423f5108dbc2c7f142cd56c8373a
hs-1971261-kimjeongseok <53766785+hs-1971261-kimjeongseok@users.noreply.github.com>
2024-06-20 22:45:32 +0900

    4 비피하기

--- Assets/Scripts/Games/Bullet.cs
+++ Assets/Scripts/Games/Bullet.cs
@@ -7,6 +7,9 @@ public class Bullet : Enemy
     public float speed = 10.0f;
     private Transform target; Vector3 direction;
 
+
+    public float acceleration = 2.0f; // 속도 증가율
+
     public bool bulletdodgegame = true;
 
     void Start()
@@ -15,21 +18,24 @@ public class Bullet : Enemy
         target = GameObject.FindGameObjectWithTag("Player").transform;
         Destroy(gameObject, 5f); // 5초 후 자동 파괴
         if (bulletdodgegame) { direction = (new Vector3(target.position.x, 0.183f, target.position.z) - transform.position).normalized; }
-        
+        else { direction = new Vector3(0,0,-1); }
     }
 
     void Update()
     {
+        if (!bulletdodgegame) { speed += acceleration * Time.deltaTime * 2; }
         if (target != null)
         {
-            
+
             transform.position += direction * speed * Time.deltaTime;
         }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (bulletdodgegame) { hitPlayer(collision); }
-        else { Destroy(this); }
+        hitPlayer(collision);
+       /* if (bulletdodgegame) { hitPlayer(collision); }
+        else { Destroy(this); }*/
     }
 }
---
+++ Assets/Scripts/Games/RainDodge.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RainDodge : Games
+{
+    public GameObject bulletPrefab;
+    public Transform[] spawnPoints; // 총알 발사 위치
+
+    private Coroutine gameCoroutine;
+
+    public override void GameStart()
+    {
+        gameCoroutine = StartCoroutine(SpawnBullets());
+    }
+
+    public override void GameStop()
+    {
+        if (gameCoroutine != null)
+        {
+            StopCoroutine(gameCoroutine);
+        }
+        // 총알 제거
+        foreach (var bullet in FindObjectsOfType<Bullet>())
+        {
+            Destroy(bullet.gameObject);
+        }
+    }
+
+    IEnumerator SpawnBullets()
+    {
+        while (true)
+        {
+            float randomX = Random.Range(-4.75f, 4.75f);
+            Vector3 spawnPosition = new Vector3(randomX + spawnPoints[1].position.x, spawnPoints[1].position.y, spawnPoints[1].position.z);
+            GameObject bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.Euler(90,0,0));
+            bullet.GetComponent<Bullet>().bulletdodgegame = false;
+            bullet.GetComponent<Bullet>().speed = 5f;
+            yield return new WaitForSeconds(1f / 3f); // 0.5초마다 총알 발사
+        }
+    }
+}
