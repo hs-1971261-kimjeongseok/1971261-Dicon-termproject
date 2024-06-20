a48587ab7c49921a5ea24219fd9258a18bbb4a10
hs-1971261-kimjeongseok <53766785+hs-1971261-kimjeongseok@users.noreply.github.com>
2024-06-21 01:40:39 +0900

    9 빠른 총알 막기

--- Assets/Scripts/Games/Bullet.cs
+++ Assets/Scripts/Games/Bullet.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bullet : Enemy
 {
@@ -13,6 +14,8 @@ public class Bullet : Enemy
     public bool bulletdodgegame = true;
     public bool centerdodgegame = false;
 
+    public bool addf = false;
+
     void Start()
     {
         //transform.Rotate(new Vector3(90,0,0));
@@ -21,15 +24,21 @@ public class Bullet : Enemy
         if (bulletdodgegame) { direction = (new Vector3(target.position.x, 0.183f, target.position.z) - transform.position).normalized; }
         else if (centerdodgegame) { direction = new Vector3(Random.Range(-1f,1f), 0f, Random.Range(-1f, 1f)); }
         else { direction = new Vector3(0,0,-1); }
+
+        if(addf) this.GetComponent<Rigidbody>().AddForce(direction.normalized * speed* 40f);
     }
 
     void Update()
     {
         if (!bulletdodgegame && !centerdodgegame) { speed += acceleration * Time.deltaTime * 2; }
-        if (target != null)
+        if (!addf)
         {
+            if (target != null)
+            {
 
-            transform.position += direction * speed * Time.deltaTime;
+                transform.position += direction * speed * Time.deltaTime;
+            }
+
         }
 
     }
@@ -39,5 +48,10 @@ public class Bullet : Enemy
         hitPlayer(collision);
        /* if (bulletdodgegame) { hitPlayer(collision); }
         else { Destroy(this); }*/
+       if(collision.transform.tag == "Wall" && addf)
+        {
+            direction = new Vector3(-1f* direction.x, -1f * direction.y, -1f * direction.z);
+            this.GetComponent<Rigidbody>().AddForce(direction * speed * 80f * 2f);
+        }
     }
 }
